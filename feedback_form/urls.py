84917d0643d0e7f597f9a2e26f552c134d2b01d4
dead23angel <dead_hart_angel@mail.ru>
2014-03-06 07:42:36 +0700

    Update urls.py

--- feedback_form/urls.py
+++ feedback_form/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url, patterns
 from feedback_form import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.ContactFormView.as_view(), name="feedback"),
-	url(r'^$', views.ContactFormView.as_view(), name="completed"),    
+                       url(r'^$', views.ContactFormView.as_view(), name="feedback"),
+                       url(r'^$', views.CompletedPage.as_view(), name="completed"),
 )

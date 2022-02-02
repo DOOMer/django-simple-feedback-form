2a6b7c0806a2f8d33221f5710d178c2906b46dcd
DOOMer <doomer3d@gmail.com>
2022-02-02 15:30:44 +0300

    Release 2.0.1

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ install_requires = \
 
 setup_kwargs = {
     'name': 'django-simple-feedback-form',
-    'version': '2.0',
+    'version': '2.0.1',
     'description': 'This is a Django application for providing a simple form for users to send emails to the administrators of your django-based site.',
     'long_description': '===========================\nDjango Simple Feedback Form\n===========================\n\nThis `Django <http://djangoproject.com>`_ app has for provide simple form for sending mail forom users to admins your django-based site.\n\nRequirements\n------------\n\nDjango 3.0+\n\n\nInstallation and configuration\n------------------------------\n\nSee `setup part`_ in documentation.\n\n.. _setup part: https://django-simple-feedback-form.readthedocs.io/en/latest/setup.html\n\nUsage\n-----\n\nSee `usage part`_ in documentation.\n\n.. _usage part: https://django-simple-feedback-form.readthedocs.io/en/latest/usage.html\n\n\nLicense\n-------\n\nLicensed under BSD license. See `license link`_ in documentation.\n\n.. _license link: LICENSE.rst',
     'author': 'Artem Galichkin',
@@ -27,7 +27,7 @@ setup_kwargs = {
     'packages': packages,
     'package_data': package_data,
     'install_requires': install_requires,
-    'python_requires': '>=3.9,<4.0',
+    'python_requires': '>=3.6',
 }
 
 
